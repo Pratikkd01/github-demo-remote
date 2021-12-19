52e605c0176927a6aa0aef2fd061d3708e0995a1
Pratikkd01 <93660250+Pratikkd01@users.noreply.github.com>
2021-12-19 15:47:02 +0530

    Addition of math.py file to remote repo main

---
+++ math.py
@@ -0,0 +1,20 @@
+# Add implementation 
+def add(x,y):
+    return x+y 
+    
+# subtract implementation
+def subtract(x,y):
+    return x-y # this is on master branch
+ # Multiply implementation   
+def multiply(x,y):
+    return x*y # on bug 456 branch 
+#divide implementation    
+def divide(x,y):
+    if y ==0:    # changes doing on master branch
+return divide_by_0_error 
+else:
+return x/y       
+
+#square implementation 
+diff square(x):
+    return x*x
